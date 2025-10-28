82a4c7583973ee7c90cbf846c16a0ee907bb3680
inevitable-ank <ankitranjanyadav@gmail.com>
2025-10-28 21:11:24 +0530

    variables

--- main.go
+++ main.go
@@ -9,4 +9,24 @@ func main() {
 	fmt.Println("Hello, World!")
 
 	myutils.Add(3, 4)
+
+	var money int = 1000
+	fmt.Println("Money:", money)
+
+	var name string = "John"
+	fmt.Println("Name:", name)
+
+	var isActive bool = true
+	fmt.Println("Is Active:", isActive)
+
+	isActive = false
+
+	const pi float64 = 3.14
+	fmt.Println("Pi:", pi)
+
+	const piApprox = 22.0 / 7.0
+	fmt.Println("Pi Approximation:", piApprox);
+
+	welcomeMessage := "Welcome to Go!"
+	fmt.Println(welcomeMessage);
 }
