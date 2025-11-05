0a3c52eeb00e67032a0a0487ba96230ca63d96ea
inevitable-ank <ankitranjanyadav@gmail.com>
2025-11-06 02:46:27 +0530

    Pointer

---
+++ Pointer/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func ChangeValueByaddress(ptr *int) {
+	*ptr = 100
+}
+
+func main() {
+	var num int;
+	num = 42
+	fmt.Println("Value of num:", num)
+
+	numPtr := &num
+	fmt.Println("Address of num:", numPtr)
+	fmt.Println("Value at address of num:", *numPtr)
+
+
+	ChangeValueByaddress(numPtr)
+	fmt.Println("Value of num after ChangeValueByaddress:", num)
+}
